e1b3e1ff6ac12da4959e5bc9e028f424938c63ae
mattiamarilli <mattiamarilli@gmail.com>
2022-06-29 09:55:09 +0200

    a working dockerized skeleton for faceart-api

---
+++ src/controllers/PaintingsController.php
@@ -0,0 +1,26 @@
+<?php
+
+
+class PaintingsController {
+
+    // GET /paintings
+    static function getPaintings($req, $res, $service, $app){
+
+        $stm = $app->db->prepare('SELECT * FROM paintings');
+        $stm->execute();
+        $dbres = $stm->fetchAll(PDO::FETCH_ASSOC);
+
+        $data = array_map(function($entry){
+            return [
+                'id' => +$entry['idpainting'],
+                'nome' => $entry['name'],
+            ];
+        }, $dbres);
+        $res->json($data);
+
+    }
+
+    
+}
+
+?>
