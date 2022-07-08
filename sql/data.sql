173624155924263043c87c6d4d8f74ed5cd10bc8
jackponzo <giacomo.ponzuoli@stud.unifi,it>
2022-07-08 12:16:02 +0200

    insert sql data

--- sql/data.sql
+++ sql/data.sql
@@ -20,12 +20,40 @@ VALUES ('Antonello da Messina', 'https://it.wikipedia.org/wiki/Antonello_da_Mess
 INSERT INTO authors(name_author, infourl_author)
 VALUES ('Tiziano Vecellio', 'https://it.wikipedia.org/wiki/Tiziano_Vecellio');
 
+INSERT INTO authors(name_author, infourl_author)
+VALUES ('Andy Warhol', 'https://it.wikipedia.org/wiki/Andy_Warhol');
+
+INSERT INTO authors(name_author, infourl_author)
+VALUES ('Gustave Courbet', 'https://it.wikipedia.org/wiki/Gustave_Courbet');
+
+INSERT INTO authors(name_author, infourl_author)
+VALUES ('Sandro Botticelli', 'https://it.wikipedia.org/wiki/Sandro_Botticelli');
+
 --PAINTINGS 
 
 INSERT INTO paintings(name,year,location,imgurl,infourl,description,id_author) 
-VALUES ('Gioconda',1503,'Louvre, Parigi','https://mattiamarilli.github.io/faceapi-img/gioconda.jpeg','https://it.wikipedia.org/wiki/Gioconda','A wonderful painting by Leonardo',1);
+VALUES ('Gioconda',1503,'Museo del Louvre, Parigi','https://mattiamarilli.github.io/faceapi-img/gioconda.jpeg','https://it.wikipedia.org/wiki/Gioconda','A wonderful painting by Leonardo Da Vinci',1);
 
 INSERT INTO paintings(name,year,location,imgurl,infourl,description,id_author) 
-VALUES ('Scudo con testa di Medusa',1598,'Uffizi, Firenze','https://mattiamarilli.github.io/faceapi-img/medusa.jpg','https://it.wikipedia.org/wiki/Scudo_con_testa_di_Medusa','A wonderful painting by Caravaggio',2);
+VALUES ('Scudo con testa di Medusa',1598,'Galleria degli Uffizi, Firenze','https://mattiamarilli.github.io/faceapi-img/medusa.jpg','https://it.wikipedia.org/wiki/Scudo_con_testa_di_Medusa','A wonderful painting by Caravaggio',2);
 
+INSERT INTO paintings(name, year, location, imgurl, infourl, description, id_author)
+VALUES ('Dama col liocorno', 1505, 'Galleria Borghese, Roma', 'https://mattiamarilli.github.io/faceapi-img/damaLiocorno.jpg', 'https://it.wikipedia.org/wiki/Dama_col_liocorno', 'A wonderful painting by Raffaello Sanzio', 4);
 
+INSERT INTO paintings(name, year, location, imgurl, infourl, description, id_author)
+VALUES ('La Bella', 1536, 'Galleria Palatina, Firenze', 'https://mattiamarilli.github.io/faceapi-img/laBella.jpg', 'https://it.wikipedia.org/wiki/La_Bella', 'A wonderful painting by Tiziano Vecellio', 6);
+
+INSERT INTO paintings(name, year, location, imgurl, infourl, description, id_author)
+VALUES ("Ritratto d'ignoto marinaio", 1476, 'Museo Mandralisca, Cefalù', 'https://mattiamarilli.github.io/faceapi-img/ignotoMarinaio.jpg', 'https://it.wikipedia.org/wiki/Ritratto_d%27ignoto_marinaio', 'A wonderful painting by Antonello da Messina', 5);
+
+INSERT INTO paintings(name, year, location, imgurl, infourl, description, id_author)
+VALUES ('Dittico di Marilyn', 1962, 'Tate, Regno Unito', 'https://mattiamarilli.github.io/faceapi-img/marilyn.jpg', 'https://it.wikipedia.org/wiki/Marilyn_Monroe', 'A wonderful painting by Andy Warhol', 7);
+
+INSERT INTO paintings(name, year, location, imgurl, infourl, description, id_author)
+VALUES ('Le Désésperé', 1845, 'Conseil Investissement Art BNP Paribas', 'https://mattiamarilli.github.io/faceapi-img/uomoDisperato.jpg', 'https://en.wikipedia.org/wiki/Le_D%C3%A9sesp%C3%A9r%C3%A9', 'A wonderful painting by Gustave Courbet', 8);
+
+INSERT INTO paintings(name, year, location, imgurl, infourl, description, id_author)
+VALUES ('Autoritratto Van Gogh', 1889, "Museo d'Orsay, Parigi", 'https://mattiamarilli.github.io/faceapi-img/vanGogh.jpg', 'https://it.wikipedia.org/wiki/Autoritratto_(Van_Gogh_1889)', 'A wonderful painting by Van Gogh', 3);
+
+INSERT INTO paintings(name, year, location, imgurl, infourl, description, id_author)
+VALUES ('Nascita di Venere', 1485, 'Galleria degli Uffizi, Firenze', 'https://mattiamarilli.github.io/faceapi-img/venere.jpg', 'https://it.wikipedia.org/wiki/Nascita_di_Venere', 'A wonderful painting by Sandro Botticelli', 9);
