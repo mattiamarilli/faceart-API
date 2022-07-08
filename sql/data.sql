20ef2bd721fc4826d289d88afc7240ef81c8014c
jackponzo <giacomo.ponzuoli@stud.unifi,it>
2022-07-08 10:38:04 +0200

    fix

--- sql/data.sql
+++ sql/data.sql
@@ -14,5 +14,5 @@ INSERT INTO paintings(name,year,location,imgurl,infourl,description,id_author)
 VALUES ('Gioconda',1503,'Louvre, Parigi','https://mattiamarilli.github.io/faceapi-img/gioconda.jpeg','https://it.wikipedia.org/wiki/Gioconda','A wonderful painting by Leonardo',1);
 
 INSERT INTO paintings(name,year,location,imgurl,infourl,description,id_author) 
-VALUES ('Scudo con testa di Medusa',1598,'Uffizi, Firenze','https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/Medusa_by_Caravaggio.jpg/520px-Medusa_by_Caravaggio.jpg','https://it.wikipedia.org/wiki/Scudo_con_testa_di_Medusa','A wonderful painting by Caravaggio',2);
+VALUES ('Scudo con testa di Medusa',1598,'Uffizi, Firenze','https://mattiamarilli.github.io/faceapi-img/medusa.jpg','https://it.wikipedia.org/wiki/Scudo_con_testa_di_Medusa','A wonderful painting by Caravaggio',2);
 
